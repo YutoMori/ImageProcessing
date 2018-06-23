37c8193a4dda5dc088a28d10f7ee75dcbbf7e47a
Yuto Mori <b1016206@fun.ac.jp>
2018-06-23 20:27:09 +0900

    added author name added histogram

--- pattern.py
+++ pattern.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# author : Yuto Mori
 
 import cv2
 import numpy as np
@@ -24,7 +25,7 @@ center = length/2
 filter_low = np.zeros(img_gray.shape)
 
 # ローパスフィルタの設定
-R = 60
+R = 50
 for i in range(0, length):
     for j in range(0, length):
         if(i-center)*(i-center) + (j-center)*(j-center) < R*R:
@@ -42,6 +43,21 @@ ifft_img = np.abs(ifft_img)
 # パワースペクトル計算
 mag_low = 20 * np.log(np.abs(lowpass_img))
 
+### ヒストグラム作成
+hist_lenna = [0] * 256
+hist_lowlenna = [0] * 256
+
+# 輝度値に対応する配列に1を足していく
+for i in range(0, len(img_gray)):
+    for j in range(0, len(img_gray[0])):
+        hist_lenna[img_gray[i, j]] += 1
+
+int_ifft_img = np.array(ifft_img, dtype='int64')
+for i in range(0, len(int_ifft_img)):
+    for j in range(0, len(int_ifft_img[0])):
+        hist_lowlenna[int_ifft_img[i, j]] += 1
+
+
 ### 画像描写について
 plt.figure()
 plt.subplot(121),plt.imshow(img_gray, cmap='gray')
@@ -54,5 +70,19 @@ plt.subplot(121),plt.imshow(mag_low, cmap='gray')
 plt.title('FFT Image through Low Pass Filter'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(ifft_img, cmap='gray')
 plt.title('Output Image'), plt.xticks([]), plt.yticks([])
+
+plt.figure()
+plt.title("Histgram of Lenna")
+plt.plot(hist_lenna)
+plt.xlabel("Pixel value")
+plt.ylabel("Number of pixels")
+plt.xlim(0, 255)
+
+plt.figure()
+plt.title("Histgram of Lenna throgh low pass")
+plt.plot(hist_lowlenna)
+plt.xlabel("Pixel value")
+plt.ylabel("Number of pixels")
+plt.xlim(0, 255)
 
 plt.show()
